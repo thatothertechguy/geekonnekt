37dd1a4049c64592fbfe788003870d56139a805e
Sam Magee <sam@thatothertechguy.com>
2012-08-10 22:51:39 -0500

    Redo

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Geekonnekt::Application.config.secret_token = '1f01be6110580d8b67745547d109dfc9bba503026ec618c64b22eb0503dba502b0f83cbaa3f7b6cf6301a0b12c2191f93a71f420cc1fa1ce8b31d3cd3008889d'
