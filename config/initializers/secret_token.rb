5b23c071b0daeac6567aab008a7b7bd0947b341d
Sam Magee <sam@thatothertechguy.com>
2012-08-10 23:10:42 -0500

    Add rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Geekonnekt::Application.config.secret_token = 'c3316f3a4ea67887ce574a07acd1a5cca643142f2e629b793b849ae924f99c73416afcddb2a24677ca8d63622e708d4e44fa79e142d0aa6c694e63ea65038454'
